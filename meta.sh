cee8ce80e16255ebaa9a2a1bca899a88e7b8af48
init-zhang <init-zhang@proton.me>
2026-02-28 11:49:12 +0000

    Replace positional parameters with variable

--- meta.sh
+++ meta.sh
@@ -5,14 +5,20 @@ Missing arguments will be prompted for"
     exit
 fi
 
-[[ -z "$1" ]] && read -p "File to change: " $1
-[[ -z "$2" ]] && read -p "Music title: " $2
-[[ -z "$3" ]] && read -p "Music artist: " $3
+file=$1
+title=$2
+artist=$3
+album=$4
 
-if [[ ! -e "$1" ]]; then
+[[ -z "$file" ]] && read -p "File to change: " file
+[[ -z "$title" ]] && read -p "Music title: " title
+[[ -z "$artist" ]] && read -p "Music artist: " artist
+[[ -z "$album" ]] && read -p "Music album (leave empty if same title): " album
+
+if [[ ! -e "$file" ]]; then
     echo "File doesn't exit"
     exit
 fi
 
-ffmpeg -i "$1" -map_metadata -1 -metadata title="$2" -metadata artist="$3" -metadata album="${4:-$2}" -codec copy "temp-$1"
-mv "temp-$1" "$1"
+ffmpeg -i "$file" -map_metadata -1 -metadata title="$title" -metadata artist="$artist" -metadata album="${album:-$title}" -codec copy "temp-$file"
+mv "temp-$file" "$file"
